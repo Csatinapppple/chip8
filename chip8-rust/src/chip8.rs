252bed4b869acc64e22c65a9f78da58005375c9c
Csatinapppple <88691457+Csatinapppple@users.noreply.github.com>
2023-07-09 22:14:45 -0300

    Add files via upload

--- chip8-rust/src/chip8.rs
+++ chip8-rust/src/chip8.rs
@@ -12,8 +12,8 @@ use {
         },
         fs::File,
     },
-    rand::Rng,
 };
+use crate::chip8::rand::Rng;
 
 pub const FONTS: [[u8; 5]; 16] = [
     //col line
@@ -37,7 +37,7 @@ pub const FONTS: [[u8; 5]; 16] = [
 //&FONTS[0][..]
 
 //currently frame limiting just sleeps for 60hz
-const SIXTY_HERTZ: Duration = time::Duration::from_micros(16670);
+pub const SIXTY_HERTZ: Duration = time::Duration::from_micros(16670);
 
 pub struct CPU {
     pub mem: [u8; 0x1000],
@@ -49,6 +49,7 @@ pub struct CPU {
     pub pc: usize,//program counter
     pub stack: [usize; 16],
     pub sp: usize,//stack pointer
+    pub keys: [bool; 16],
 }
 
 
@@ -77,26 +78,6 @@ impl CPU{
         self.pc = nnn;
     }
 
-    //3XNN & 4XNN skips if the eq flag is true and cond is met so is the inverse
-    pub fn skip_if(&mut self,x: usize, nn: u8, eq: bool){
-        let cond = self.v[x] == nn;
-        if cond && eq{
-            self.pc += 2;
-        }else if !cond && !eq {
-            self.pc += 2;
-        }
-    }
-    
-    //5XY0 & 9XY0 same as skip_if but for registers
-    pub fn skip_if_reg(&mut self,x: usize,y: usize,eq: bool){
-        let cond = self.v[x] == self.v[y];
-        if cond && eq{
-            self.pc += 2;
-        }else if !cond && !eq{
-            self.pc += 2;
-        }
-    }
-    
     //8XY4 add vX with vY and set vF to 1 in overflow
     pub fn add_x_y(&mut self,x:usize,y:usize){
         let cmp = self.v[x];
@@ -111,23 +92,23 @@ impl CPU{
         in case where the first operand is bigger than the second operand ie.
         (n+1) - n it sets vF to 1 and the inverse sets it to 0
      */ 
-    pub fn sub_x_y(&mut self, x:usize,y:usize,inv:bool){
-        if inv {
-            if self.v[y] >= self.v[x] {
-                self.v[0xF] = 1;
-            } else{
-                self.v[0xF] = 0;
-            }
-            self.v[x] = self.v[y] - self.v[x];
+    //8XY5
+    pub fn sub_x_with_y(&mut self, x:usize,y:usize){
+        if self.v[x] >= self.v[y] {
+            self.v[0xF] = 1;
+        } else{
+            self.v[0xF] = 0;
         }
-        else {
-            if self.v[x] >= self.v[y] {
-                self.v[0xF] = 1;
-            } else{
-                self.v[0xF] = 0;
-            }
-            self.v[x] -= self.v[y];
+        self.v[x] -= self.v[y];
+    }
+    //8XY7
+    pub fn sub_x_with_y_x(&mut self, x:usize,y:usize){
+        if self.v[y] >= self.v[x] {
+            self.v[0xF] = 1;
+        } else{
+            self.v[0xF] = 0;
         }
+        self.v[x] = self.v[y] - self.v[x];
     }
 
     /*
@@ -137,28 +118,27 @@ impl CPU{
         works for the last bit shifted to the right and the first bit to the
         left
     */
-    pub fn shift(&mut self, x:usize, y:usize, right:bool){
-        self.v[x] = self.v[y];
-        match right {
-            true => {
-                if self.v[x] & 0b00000001 == 1{
-                    self.v[0xf] = 1;
-                }
-                else {
-                    self.v[0xf] = 0;
-                }
-                self.v[x] >> 1;
-            },
-            false => {
-                if self.v[x] & 0b10000000 == 1{
-                    self.v[0xf] = 1;
-                }
-                else {
-                    self.v[0xf] = 0;
-                }
-                self.v[x] << 1;
-            },
+    //8XY6
+    pub fn shift_right(&mut self, x:usize, y:usize){
+        self.v[x] = self.v[y];    
+        if self.v[x] & 0b00000001 == 1{
+            self.v[0xf] = 1;
         }
+        else {
+            self.v[0xf] = 0;
+        }
+        self.v[x] >>= 1;
+    }
+    //8XYE
+    pub fn shift_left(&mut self, x:usize, y:usize){
+        self.v[x] = self.v[y];
+        if self.v[x] & 0b10000000 == 1{
+            self.v[0xf] = 1;
+        }
+        else {
+            self.v[0xf] = 0;
+        }
+        self.v[x] <<= 1;
     }
 
     //CXNN generates random number and puts the result in vX after ANDing with nn
@@ -167,14 +147,6 @@ impl CPU{
         self.v[x] = random_number & nn;
     }
 
-    //DYXN display,draw
-    pub fn draw(&mut self, y:usize,x:usize,n:usize){
-        for y_screen in 0..n{
-            let line: u8 = self.mem[self._I+y_screen];
-            for x_screen in 0..8{
-            }
-        }
-    }
     pub fn decode(&mut self) {
         let opcode: u16 = self.fetch();
         self.pc += 2;
@@ -193,9 +165,9 @@ impl CPU{
             },
             0x1000 => self.pc = nnn, //1NNN set pc to NNN (jump)
             0x2000 => self.ret_from_sub_at_addr(nnn),
-            0x3000 => self.skip_if(x,nn,true),//skip if equal
-            0x4000 => self.skip_if(x,nn,false),//skip if different
-            0x5000 => self.skip_if_reg(x,y,true),
+            0x3000 => self.pc += if self.v[x] == nn {2} else {0},//3XNN skip if equal
+            0x4000 => self.pc += if self.v[x] != nn {2} else {0},//4XNN skip if different 
+            0x5000 => self.pc += if self.v[x] == self.v[y] {2} else {0},//5XY0 skip if equal
             0x6000 => self.v[x] = nn, //6XNN set vX to NN
             0x7000 => self.v[x] += nn,//7XNN add NN to vX without setting vF
             0x8000 => match opcode & 0x000F {
@@ -205,17 +177,22 @@ impl CPU{
                 0x0002 => self.v[x]&=self.v[y],//8XY2 AND vX with vY
                 0x0003 => self.v[x]^=self.v[y],//8XY3 XOR vX with vY
                 0x0004 => self.add_x_y(x,y),
-                0x0005 => self.sub_x_y(x,y,false),
-                0x0006 => self.shift(x,y,true),
-                0x0007 => self.sub_x_y(x,y,true),
-                0x000E => self.shift(x,y,false),
+                0x0005 => self.sub_x_with_y(x,y),
+                0x0006 => self.shift_right(x,y),
+                0x0007 => self.sub_x_with_y_x(x,y),
+                0x000E => self.shift_left(x,y),
                 _ => unreachable!(),
             }
-            0x9000 => self.skip_if_reg(x,y,false),
+            0x9000 => self.pc += if self.v[x] != self.v[y] {2} else {0},//9XY0 skip if different
             0xA000 => self._I = nnn, //ANNN set index to NNN
             0xB000 => self.pc = nnn + usize::from(self.v[0]), //BNNN COSMAC VIP jumps to nnn + v0
-            0xC000 => self.random(x,nn),
-            //0xD000 => self.put_sprite()
+            0xC000 => self.random(x,nn),//CXNN get random number
+            0xD000 => self.draw(x,y,n), //DXYN draw display sprite
+            0xE000 => match opcode & 0x00FF {
+                0x009E => self.pc += if self.keys[usize::from(self.v[x])]{2}else{0}, //EX9E skip if vX key is true
+                0x00A1 => self.pc += if !self.keys[usize::from(self.v[x])]{2}else{0}, //EXA1 skip if vX key is false
+                _ => unreachable!(),
+            }
             _ => unreachable!(),
 
         }
@@ -227,56 +204,31 @@ impl CPU{
     }
 
 
-
-    /*put_sprite(x_start,y_start,)DXYN
-                |
-                v x_start_max
-    |       5 6 7 8 9 0 1 2 3 64      iterates and replaces each byte
-          5 0 0 0 0 0 0 0 0 0 0       with the binary of the byte
-          6 0 0 0 0 0 0 0 0 0 0       y_start_max = 32 - sprite.len()
-          7 0 0 0 0 0 0 0 0 0 0
-          8 0 0 0 0 0 0 0 0 0 0
-          9 0 0 0 0 0 0 0 0 0 0
-          0 0 0 0 0 0 0 0 0 0 0
-          1 0 0 0 0 0 0 0 0 0 0
-         32 0 0 0 0 0 0 0 0 0 0
-
-    */
-    pub fn put_sprite(&mut self, sprite: &[u8], x_start: usize, y_start: usize) {
-        if x_start > 64 - 8 && y_start > 32 - sprite.len() {
-            println!(
-                "put_sprite(..,x_start = {},y_start = {}) ERROR X AND Y AXIS OUT OF BOUNDS",
-                x_start, y_start
-            );
-            return;
-        } else if x_start > 64 - 8 {
-            println!(
-                "put_sprite(..,x_start = {},y_start = {}) ERROR X AXIS OUT OF BOUNDS",
-                x_start, y_start
-            );
-            return;
-        } else if y_start > 32 - sprite.len() {
-            println!(
-                "put_sprite(..,x_start = {},y_start = {}) ERROR Y AXIS OUT OF BOUNDS",
-                x_start, y_start
-            );
-            return;
-        }
-        //i + 7
-        //to extract the last bit, bit-shift 7 times to the right
-        //to extract the first bit, AND with 0b00000001
-        //(0b10011001 >> 7-n) & 0x01
-        let mut i = 0usize;
-        for y in y_start..y_start + sprite.len() {
-            for x in x_start..x_start + 8 {
-                self.screen[ y ][ x ] = (sprite[y - y_start] >> 7 - i) & 0x01;
-                println!("{} {}", i, (sprite[y - y_start] >> 7 - i) & 0x01);
-                i += 1;
+    //DXYN display,draw
+    pub fn draw(&mut self, x_reg:usize,y_reg:usize,n:usize){
+        let mut i: usize = 0;
+        let mut y_coord: usize = self.v[y_reg].into();
+        let mut x_coord: usize = self.v[x_reg].into();//height,width = y_reg,x_reg
+        println!("x = {} y = {} n = {}",y_coord,x_coord,n);
+        for y in y_coord..y_coord+n {
+            //offset of n present in y_coord+n is the amount of bytes in the sprite starting from I
+            for x in x_coord..x_coord+8 {
+                if x >= 64 || y >= 32 {
+                    continue;
+                }
+                else{
+                    self.screen[y][x] = (self.mem[self._I + y - y_coord] >> 7 - i) & 0x1;
+                    i+=1;
+                    println!(
+                        "[{}][{}]",
+                        y,x
+                    );
+                }
+                
             }
-            i = 0;
+            i=0;
         }
     }
-
 
     //Initializes variables to default settings
     pub fn init() -> CPU{
@@ -290,6 +242,7 @@ impl CPU{
             pc: 0x200,
             stack: [0; 16],
             sp: 0,
+            keys: [false; 16],
         };
 
         for y in 0..16{
--- chip8-rust/src/main.rs
+++ chip8-rust/src/main.rs
@@ -12,15 +12,14 @@ fn main() {
     //initiate cpu
     let mut c8 = chip8::CPU::init();
     //load rom into cpu
-    c8.load_file("rom/ibm_logo.bin");
-    
+    c8.load_file("rom/pong.bin");
     //render and call emulate cycle
-    c8.put_sprite(&chip8::FONTS[0xa][..],0,0);
     loop {
         println!(
             "fetch = {:#06x}",
             c8.fetch()
         );
+        c8.decode();
         for y in 0..32 {
             for x in 0..64 {
                 match c8.screen[y][x] {
@@ -30,6 +29,6 @@ fn main() {
             }
             println!("");
         }
-        thread::sleep(time::Duration::from_secs(10));
+        thread::sleep(chip8::SIXTY_HERTZ);
     }
 }
